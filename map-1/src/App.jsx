7a0c654e9f6f08da29192af2a62fd13b71e2a5ef
KOUDJALE  Aimé Dam-some <aime.koudjale@etu.iai-togo.tg>
2026-08-03 11:48:42 +0000

    premier commit

---
+++ map-1/src/App.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+/*const listeF = [
+  { title :"Pomme",
+    isFruit:true,
+    id:1   
+},
+{
+  title:"Banane",
+  isFruit:true,
+  id:2
+},
+{
+  title:"choux",
+  isFruit:false,
+  id:3
+},
+{
+  title:"orange",
+  isFruit:true,
+  id:4
+}
+
+]
+
+const listitem = listeF.map((produit) =>
+  <li key={produit.id}
+          style={{
+            color: produit.isFruit ? "green":"red"
+          }}>
+    {produit.title}
+  </li>)
+
+const noms = ["Jean", "Marie", "Paul"]
+
+const lnom = noms.map((n)=>
+<li>
+ Bonjour {n}
+</li>)
+const nombres = [1,2,3,4,5]
+const double = nombres.map((n)=>n*2)
+
+const villes = ["lomé","paris","tokyo"]
+const up= villes.map((n)=>n.toUpperCase())
+
+const x =["Chat","Chien","Oiseau"]
+const y = x.map((n)=> n +"💔")
+
+alert(y)
+
+const users = [
+ {id:1, nom:"Aime"},
+ {id:2, nom:"John"},
+ {id:3, nom:"Alice"}
+]
+
+const name = users.map((n)=> n.nom)
+alert(name)*/
+const idt[
+{
+nom:"Aime",
+age:18
+}
+] 
+
+
+
+export default function App(){
+  return<>
+         {y}
+  </>
+}
