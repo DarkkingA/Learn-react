4bebaf10a1ccb9c49020cbd2d8389e6e5733f7ac
KOUDJALE  Aimé Dam-some <aime.koudjale@etu.iai-togo.tg>
2026-08-03 17:37:10 +0000

    test 1
    today

---
+++ Todo-liste/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+
+const [tache,settache]=useState("")
+
+function Addtache(){
+  <input type="text"
+  value={texte}
+  onChange={(e)=>settache(e.target.value)}
+  placeholder="" />
+}
+
+
+export default function App(){
+  return {container}
+}
