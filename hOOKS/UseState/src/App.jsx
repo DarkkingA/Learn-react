c8a1f6629f250b3c70238756e0129db9140bea6a
KOUDJALE  Aimé Dam-some <aime.koudjale@etu.iai-togo.tg>
2026-08-06 20:57:53 +0000

    premier pas avec usestate

--- hOOKS/UseState/src/App.jsx
+++ hOOKS/UseState/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-[count,setCount]=useState(0)
+import { useState } from 'react';
 
+
+
+
+export default function App() {
+
+    
+const [count,setCount]=useState(0)
 function getB(){
-  
+    setCount(count +1 )
 }
-export default App(){
-
-  
-
+    return( <>
+    <h1> Premier pas avec useState </h1>
+    <button onClick={getB} >Count1 {count} 
+    </button>    
+    </>)
 }
